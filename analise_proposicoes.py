137955ddb703324ceb6efa2420df45b272e2516e
Amanda Veloso <139825936+amandaveloso@users.noreply.github.com>
2024-02-26 11:48:07 -0300

    Criado usando o Colaboratory

---
+++ analise_proposicoes.py
@@ -0,0 +1,28 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyN329Q07j2VrYhpz+WReB0o"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "OJ0XEllT4sBn"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
